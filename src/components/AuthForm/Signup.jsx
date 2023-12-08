88b491a80c3bc6b7693ecfa560927d224c1a3c5e
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-08 18:50:40 +0100

    setup signup

---
+++ src/components/AuthForm/Signup.jsx
@@ -0,0 +1,93 @@
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import {
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import useSignupWithEmailAndPassword from "../../hooks/useSignupWithEmailAndPassword";
+
+function Signup() {
+  const [inputs, setInputs] = useState({
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const { loading, error, signup } = useSignupWithEmailAndPassword();
+  return (
+    <>
+      <Input
+        placeholder="Email"
+        fontSize={14}
+        type="email"
+        name="email"
+        size={"sm"}
+        onChange={handleChange}
+      />
+      <Input
+        placeholder="Username"
+        fontSize={14}
+        type="text"
+        name="username"
+        size={"sm"}
+        onChange={handleChange}
+      />
+      <Input
+        placeholder="Fullname"
+        fontSize={14}
+        type="text"
+        name="fullname"
+        size={"sm"}
+        onChange={handleChange}
+      />
+      <InputGroup>
+        <Input
+          placeholder="Password"
+          fontSize={14}
+          type={showPassword ? "text" : "password"}
+          name="password"
+          size={"sm"}
+          onChange={handleChange}
+        />
+        <InputRightElement h={"full"}>
+          <Button
+            variant={"ghost"}
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+      <Button
+        isLoading={loading}
+        onClick={() => signup(inputs)}
+        w={"full"}
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
+}
+
+export default Signup;
---
+++ src/hooks/useSignupWithEmailAndPassword.js
@@ -0,0 +1,52 @@
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import useShowToast from "./useShowToast";
+import useAuthStore from "../store/authStore";
+import { useNavigate } from "react-router-dom";
+
+function useSignupWithEmailAndPassword() {
+  const [createUserWithEmailAndPassword, , loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
+  const navigate = useNavigate();
+
+  const signup = async ({ fullname, username, email, password }) => {
+    if (!fullname || !username || !email || !password) {
+      showToast("Error", "Please fill all the fields", "error");
+      return;
+    }
+    try {
+      const newUser = await createUserWithEmailAndPassword(email, password);
+      if (!newUser && error) {
+        showToast("Error", error.message, "error");
+        return;
+      }
+      if (newUser) {
+        const userDoc = {
+          uid: newUser.user.uid,
+          email,
+          username,
+          fullname,
+          bio: "",
+          profilePicURL: "",
+          followers: [],
+          followerings: [],
+          posts: [],
+          createAt: Date.now(),
+        };
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
+        navigate("/");
+      }
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
+
+  return { loading, error, signup };
+}
+
+export default useSignupWithEmailAndPassword;
