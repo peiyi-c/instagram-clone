3479d59b04b6230a36d8b24baf15054fa643aeb2
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-25 10:17:21 +0800

    add logout, user page link

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,23 +1,39 @@
-import { Avatar, Text, Flex, Link } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Avatar, Text, Flex, Button } from "@chakra-ui/react";
+import useLogout from "../../hooks/useLogout";
+import useAuthStore from "../../store/authStore";
+import { Link } from "react-router-dom";
+
 function SuggestedHeader() {
+  const { logout, loading } = useLogout();
+  const authUser = useAuthStore((state) => state.user);
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
-      <Avatar name="penny c." size={"md"} src="/profilepic.png" />
-      <Text fontSize={12} fontWeight={"bold"}>
-        philedelp.sd_
-      </Text>
-      <Link
-        as={RouterLink}
-        to={"/auth"}
+      <Flex alignItems={"center"} gap={2}>
+        <Link to={`${authUser.username}`}>
+          <Avatar size={"md"} src={authUser.profilePicURL} />
+        </Link>
+        <Link to={`${authUser.username}`}>
+          <Text fontSize={12} fontWeight={"bold"}>
+            {authUser.username}
+          </Text>
+        </Link>
+      </Flex>
+
+      <Button
+        onClick={logout}
+        isLoading={loading}
+        size={"xs"}
+        background={"transparent"}
+        _hover={{
+          background: "transparent",
+        }}
         fontSize={14}
         fontWeight={"medium"}
         color={"blue.400"}
-        style={{ textDecoration: "none" }}
         cursor={"pointer"}
       >
         Log out
-      </Link>
+      </Button>
     </Flex>
   );
 }
