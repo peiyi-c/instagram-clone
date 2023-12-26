1615a0566797f32b6dbb2cd2474c992ea4bfd6d8
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-26 17:11:55 +0800

    add sidebarItems

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,10 @@
-import { Avatar, Box, Flex, Link, Tooltip, Button } from "@chakra-ui/react";
+import { Box, Flex, Link, Tooltip, Button } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
-import {
-  CreatePostLogo,
-  InstagramLogo,
-  InstagramMobileLogo,
-  NotificationsLogo,
-  SearchLogo,
-} from "../../assets/logos";
-import { AiFillHome } from "react-icons/ai";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/logos";
+import SidebarItems from "./SidebarItems";
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
-function Sidebar() {
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={24} />,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo />,
-      text: "Search",
-    },
-    { icon: <NotificationsLogo />, text: "Notifications" },
-    { icon: <CreatePostLogo />, text: "Create" },
-    {
-      icon: <Avatar size={"sm"} name="Bella Lee" src="./profilepic.png" />,
-      text: "Profile",
-      link: "/belleeelee",
-    },
-  ];
+const Sidebar = () => {
   const { logout, loading } = useLogout();
   return (
     <Box
@@ -72,33 +48,7 @@ function Sidebar() {
           <InstagramMobileLogo />
         </Link>
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={300}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                as={RouterLink}
-                to={item.link || null}
-                alignItems={"center"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: 10, md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SidebarItems />
         </Flex>
         {/* Logout */}
         <Tooltip
@@ -134,6 +84,6 @@ function Sidebar() {
       </Flex>
     </Box>
   );
-}
+};
 
 export default Sidebar;
---
+++ src/components/Sidebar/SidebarItems.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import Notifications from "./Notifications";
+import CreatePost from "./CreatePost";
+import ProfileLink from "./ProfileLink";
+import Search from "./Search";
+
+const SidebarItems = () => {
+  return (
+    <>
+      <Home />
+      <Search />
+      <Notifications />
+      <CreatePost />
+      <ProfileLink />
+    </>
+  );
+};
+
+export default SidebarItems;
