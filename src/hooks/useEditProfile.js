7b0675b32ab98768334daaa8c5e3daed32b613c9
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-26 17:14:55 +0800

    change to arrow function

--- src/hooks/useEditProfile.js
+++ src/hooks/useEditProfile.js
@@ -6,11 +6,7 @@ import { doc, updateDoc } from "firebase/firestore";
 import { firestore, storage } from "../firebase/firebase";
 import useUserProfileStore from "../store/userProfileStore";
 
-<<<<<<< HEAD
 const useEditProfile = () => {
-=======
-function useEditProfile() {
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
   const [isUpdating, setIsUpdating] = useState(false);
   const authUser = useAuthStore((state) => state.user);
   const setAuthUser = useAuthStore((state) => state.setUser);
@@ -32,11 +28,7 @@ function useEditProfile() {
         URL = await getDownloadURL(ref(storage, `profilePics/${authUser.uid}`));
       }
       const updatedUser = {
-<<<<<<< HEAD
         // ...authUser,
-=======
-        ...authUser,
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
         fullname: inputs.fullname || authUser.fullname,
         username: inputs.username || authUser.username,
         bio: inputs.bio || authUser.bio,
@@ -46,24 +38,14 @@ function useEditProfile() {
       localStorage.setItem("user-info", JSON.stringify(updatedUser));
       setAuthUser(updatedUser);
       setUserProfile(updatedUser);
-<<<<<<< HEAD
     } catch (error) {
       showToast("Error", error.message, "error");
     } finally {
-=======
-      setIsUpdating(false);
-    } catch (error) {
-      showToast("Error", error.message, "error");
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
       setIsUpdating(false);
     }
   };
 
   return { editProfile, isUpdating };
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
 
 export default useEditProfile;
--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
 import useShowToast from "./useShowToast";
 
-<<<<<<< HEAD
 const usePreviewImg = () => {
-=======
-function usePreviewImg() {
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
   const [selectedFile, setSelectedFile] = useState(null);
   const showToast = useShowToast();
   const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
@@ -29,10 +25,6 @@ function usePreviewImg() {
     }
   };
   return { selectedFile, setSelectedFile, handleImageChange };
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 23f6736714ca4bd2a5d21ae3246f5faf5bf819d6
 
 export default usePreviewImg;
