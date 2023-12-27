4ab240dc4f2455c6b04dcc478506f69b6616552d
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-27 18:57:41 +0800

    setup create posts

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,7 +1,48 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Tooltip,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  useDisclosure,
+  Textarea,
+  ModalFooter,
+  Input,
+  Button,
+  Image,
+  CloseButton,
+} from "@chakra-ui/react";
+import { BsFillImageFill } from "react-icons/bs";
 import { CreatePostLogo } from "../../assets/logos";
+import { useRef, useState } from "react";
+import usePreviewImg from "../../hooks/usePreviewImg";
+import useCreatePost from "../../hooks/useCreatePost";
+import useShowToast from "../../hooks/useShowToast";
 
 const CreatePost = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [caption, setCaption] = useState("");
+  const imageRef = useRef(null);
+
+  const { selectedFile, setSelectedFile, handleImageChange } = usePreviewImg();
+  const { isLoading, handleCreatePost } = useCreatePost();
+
+  const showToast = useShowToast();
+
+  const handlePostCreation = async () => {
+    try {
+      await handleCreatePost(selectedFile, caption);
+      onClose();
+      setCaption("");
+      setSelectedFile(null);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
   return (
     <>
       <Tooltip
@@ -13,6 +54,7 @@ const CreatePost = () => {
         display={{ base: "block", md: "none" }}
       >
         <Flex
+          onClick={onOpen}
           alignItems={"center"}
           gap={4}
           _hover={{ bg: "whiteAlpha.400" }}
@@ -25,6 +67,62 @@ const CreatePost = () => {
           <Box display={{ base: "none", md: "block" }}>Create</Box>
         </Flex>
       </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+
+        <ModalContent bg={"black"} border={"1px solid gray"}>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Textarea
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+              placeholder="Post caption..."
+            />
+
+            <Input
+              type="file"
+              hidden
+              ref={imageRef}
+              onChange={handleImageChange}
+            />
+
+            <BsFillImageFill
+              onClick={() => imageRef.current.click()}
+              style={{
+                marginTop: "15px",
+                marginLeft: "5px",
+                cursor: "pointer",
+              }}
+              size={16}
+            />
+            {selectedFile && (
+              <Flex
+                mt={5}
+                w={"full"}
+                position={"relative"}
+                justifyContent={"center"}
+              >
+                <Image src={selectedFile} alt="selected img" />
+                <CloseButton
+                  position={"absolute"}
+                  top={2}
+                  right={2}
+                  onClick={() => {
+                    setSelectedFile(null);
+                  }}
+                />
+              </Flex>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button mr={3} onClick={handlePostCreation} isLoading={isLoading}>
+              Post
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
