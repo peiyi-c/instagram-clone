3085ad79d9874566f16ad487eef4b370ece33da4
peiyi-c <pennieyoyoyo@gmail.com>
2023-12-31 23:30:54 +0800

    add updating state check

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -26,88 +26,90 @@ const ProfileHeader = () => {
   );
 
   return (
-    <Flex gap={{ base: 4, sm: 10 }} direction={{ base: "column", sm: "row" }}>
-      <AvatarGroup
-        size={{ base: "xl", md: "2xl" }}
-        justifySelf={"center"}
-        alignSelf={"flex-start"}
-        mx={"auto"}
-      >
-        <Avatar
-          src={userProfile.profilePicURL}
-          alt={userProfile.username}
-          name={userProfile.username}
-        />
-      </AvatarGroup>
-      <VStack alignItems={"flex-start"} gap={2} mx={"auto"} flex={1}>
-        <Flex
-          gap={4}
-          direction={{ base: "column", sm: "row" }}
-          justifyContent={{ base: "center", sm: "flex-start" }}
-          alignItems={"center"}
-          w={"full"}
+    !isUpdating && (
+      <Flex gap={{ base: 4, sm: 10 }} direction={{ base: "column", sm: "row" }}>
+        <AvatarGroup
+          size={{ base: "xl", md: "2xl" }}
+          justifySelf={"center"}
+          alignSelf={"flex-start"}
+          mx={"auto"}
         >
-          <Text fontSize={{ base: "sm", md: "lg" }}>
-            {userProfile.username}
-          </Text>
-          {visitingOwnProfileAndAuth && (
-            <Flex gap={4} justifyContent={"center"} alignItems={"center"}>
-              <Button
-                onClick={onOpen}
-                bg={"white"}
-                color={"black"}
-                _hover={{ bg: "whiteAlpha.800" }}
-                size={{ base: "xs", md: "sm" }}
-              >
-                Edit Profile
-              </Button>
-            </Flex>
-          )}
-          {visitingAnotherProfileAndAuth && (
-            <Flex gap={4} justifyContent={"center"} alignItems={"center"}>
-              <Button
-                onClick={handleFollowUser}
-                isLoading={isUpdating}
-                bg={"blue.500"}
-                color={"white"}
-                _hover={{ bg: "blue.600" }}
-                size={{ base: "xs", md: "sm" }}
-              >
-                {isFollowing ? "Unfollow" : "Follow"}
-              </Button>
-            </Flex>
-          )}
-        </Flex>
-        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }} mt={1}>
-          <Text fontSize={{ base: "xs", md: "sm" }}>
-            <Text as="span" fontWeight={"bold"} mr={1}>
-              {userProfile.posts.length}
+          <Avatar
+            src={userProfile.profilePicURL}
+            alt={userProfile.username}
+            name={userProfile.username}
+          />
+        </AvatarGroup>
+        <VStack alignItems={"flex-start"} gap={2} mx={"auto"} flex={1}>
+          <Flex
+            gap={4}
+            direction={{ base: "column", sm: "row" }}
+            justifyContent={{ base: "center", sm: "flex-start" }}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text fontSize={{ base: "sm", md: "lg" }}>
+              {userProfile.username}
             </Text>
-            Posts
-          </Text>
-          <Text fontSize={{ base: "xs", md: "sm" }}>
-            <Text as="span" fontWeight={"bold"} mr={1}>
-              {userProfile.followers.length}
+            {visitingOwnProfileAndAuth && (
+              <Flex gap={4} justifyContent={"center"} alignItems={"center"}>
+                <Button
+                  onClick={onOpen}
+                  bg={"white"}
+                  color={"black"}
+                  _hover={{ bg: "whiteAlpha.800" }}
+                  size={{ base: "xs", md: "sm" }}
+                >
+                  Edit Profile
+                </Button>
+              </Flex>
+            )}
+            {visitingAnotherProfileAndAuth && (
+              <Flex gap={4} justifyContent={"center"} alignItems={"center"}>
+                <Button
+                  onClick={handleFollowUser}
+                  isLoading={isUpdating}
+                  bg={"blue.500"}
+                  color={"white"}
+                  _hover={{ bg: "blue.600" }}
+                  size={{ base: "xs", md: "sm" }}
+                >
+                  {isFollowing ? "Unfollow" : "Follow"}
+                </Button>
+              </Flex>
+            )}
+          </Flex>
+          <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }} mt={1}>
+            <Text fontSize={{ base: "xs", md: "sm" }}>
+              <Text as="span" fontWeight={"bold"} mr={1}>
+                {userProfile.posts.length}
+              </Text>
+              Posts
             </Text>
-            Followers
-          </Text>
-          <Text fontSize={{ base: "xs", md: "sm" }}>
-            <Text as="span" fontWeight={"bold"} mr={1}>
-              {userProfile.followings.length}
+            <Text fontSize={{ base: "xs", md: "sm" }}>
+              <Text as="span" fontWeight={"bold"} mr={1}>
+                {userProfile.followers.length}
+              </Text>
+              Followers
             </Text>
-            Followings
-          </Text>
-        </Flex>
-        <Flex alignItems={"center"} gap={4}>
-          <Text fontSize={"sm"} fontWeight={"bold"}>
-            {userProfile.fullname}
-          </Text>
-        </Flex>
+            <Text fontSize={{ base: "xs", md: "sm" }}>
+              <Text as="span" fontWeight={"bold"} mr={1}>
+                {userProfile.followings.length}
+              </Text>
+              Followings
+            </Text>
+          </Flex>
+          <Flex alignItems={"center"} gap={4}>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              {userProfile.fullname}
+            </Text>
+          </Flex>
 
-        <Text fontSize={"sm"}>{userProfile.bio}</Text>
-      </VStack>
-      {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
-    </Flex>
+          <Text fontSize={"sm"}>{userProfile.bio}</Text>
+        </VStack>
+        {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
+      </Flex>
+    )
   );
 };
 
